368d6f226f43060c7cd427e9820a037ef6385dc0
Thomas Hrabe <thrabe@gmail.com>
2016-01-29 15:01:39 -0800

    starting to track this

---
+++ app/phps/startModel.php
@@ -0,0 +1,25 @@
+<?php
+
+	$fullSequence 	= $_POST['sequence'];
+
+	// $fullSequence = ">1A50:A|PDBID|CHAIN|SEQUENCE\nMERYENLFAQLNDRREGAFVPFVTLGDPGIEQSLKIIDTLIDAGADALELGVPFSDPLADGPTIQNANLRAFAAGVTPAQCFEMLALIREKHPTIPIGLLMYANLVFNNGIDAFYARCEQVGVDSVLVADVPVEESAPFRQAALRHNIAPIFICPPNADDDLLRQVASYGRGYTYLLSRSGVTGAENRGALPLHHLIEKLKEYHAAPALQGFGISSPEQVSAAVRAGAAGAISGSAIVKIIEKNLASPKQMLAELRSFVSAMKAASRA";
+
+	$url = 'http://pdbflex.org/fsn/php/alignSequences.php';
+	$data = array('sequence' => $fullSequence);
+
+	// use key 'http' even if you send the request to https://...
+	$options = array(
+	    'http' => array(
+	        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+	        'method'  => 'POST',
+	        'content' => http_build_query($data),
+	    ),
+	);
+	$context  = stream_context_create($options);
+
+	$masterIDsJSONString = file_get_contents($url, false, $context);
+
+
+
+?>
+
