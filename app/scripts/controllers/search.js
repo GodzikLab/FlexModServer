96c34ae48670b956410fa7018810357c80be0b7b
Mayya Sedova <mayya.sedova@gmail.com>
2016-02-01 14:06:27 -0800

    Send test data to php script, get results back.

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -8,126 +8,92 @@
  * Controller of the modFlexApp
  */
 angular.module('modFlexApp')
-    .controller('SearchCtrl', ['$scope', '$location', function ($scope, $location) {
+    .controller('SearchCtrl', ['$scope', '$location', '$http', '$templateCache',
+        function ($scope, $location, $http, $templateCache) {
             $scope.isActive = ($location.url() === "/search");
             $scope.filtLigand = false;
+            $scope.r = {};
+            var testSeq = ">1A50:A|PDBID|CHAIN|SEQUENCE\nMERYENLFAQLNDRREGAFVPFVTLGDPGIEQSLKIIDTLIDAGADALELGVPFSDPLADGPTIQNANLRAFAAGVTPAQCFEMLALIREKHPTIPIGLLMYANLVFNNGIDAFYARCEQVGVDSVLVADVPVEESAPFRQAALRHNIAPIFICPPNADDDLLRQVASYGRGYTYLLSRSGVTGAENRGALPLHHLIEKLKEYHAAPALQGFGISSPEQVSAAVRAGAAGAISGSAIVKIIEKNLASPKQMLAELRSFVSAMKAASRA";
 
-            // replace this json with server script call later
-            var result = {
-                id: 12345,
-                hits: [{
-                        "PDB": "1ujp",
-                        "chain": "A",
-                        "masterID": "1ujpA",
-                        "masterDbID": "13292",
-                        "score": "4.000e-26",
-                        "ident": 32.02,
-                        "flex": 5,
-                        "rmsd": 2.467,
-                        "size": 2,
-                        "representatives": [{
-                                "pdb": "1wxj",
-                                "chain": "A",
-                                "length": "271",
-                                "description": ">1wxj_A mol:protein length:271  tryptophan synthase alpha chain",
-                                "seqres": "MTTLEAFAKARSEGRAALIPYLTAGFPSREGFLQAVEEVLPYADLLEIGLPYSDPLGDGPVIQRASELALRKGMSVQGALELVREVRALTEKPLFLMTYLNPVLAWGPERFFGLFKQAGATGVILPDLPPDEDPGLVRLAQEIGLETVFLLAPTSTDARIATVVRHATGFVYAVSVTGVTGMRERLPEEVKDLVRRIKARTALPVAVGFGVSGKATAAQAAVADGVVVGSALVRALEEGRSLAPLLQEIRQGLQRLEANPGLKESSKKPLP",
-//                                "ligands": ["3-(INDOL-3-YL)PROPYL PHOSPHATE"],
-                                img: "http://www.rcsb.org/pdb/images/1ksr_asr_r_250.jpg"
-                            }, {
-                                "pdb": "1wxj",
-                                "chain": "A",
-                                "length": "271",
-                                "description": ">1wxj_A mol:protein length:271  tryptophan synthase alpha chain",
-                                "seqres": "MTTLEAFAKARSEGRAALIPYLTAGFPSREGFLQAVEEVLPYADLLEIGLPYSDPLGDGPVIQRASELALRKGMSVQGALELVREVRALTEKPLFLMTYLNPVLAWGPERFFGLFKQAGATGVILPDLPPDEDPGLVRLAQEIGLETVFLLAPTSTDARIATVVRHATGFVYAVSVTGVTGMRERLPEEVKDLVRRIKARTALPVAVGFGVSGKATAAQAAVADGVVVGSALVRALEEGRSLAPLLQEIRQGLQRLEANPGLKESSKKPLP",
-                                "ligands": ["3-(INDOL-3-YL)PROPYL PHOSPHATE"],
-                                img: "http://www.rcsb.org/pdb/images/1ksr_asr_r_250.jpg"
-                            }]
-                    }, {
-                        "PDB": "1ujp",
-                        "chain": "A",
-                        "masterID": "1ujpA",
-                        "masterDbID": "13292",
-                        "score": "4.000e-26",
-                        "ident": 32.02,
-                        "flex": 5,
-                        "rmsd": 2.467,
-                        "size": 2,
-                        "representatives": [{
-                                "pdb": "1wxj",
-                                "chain": "A",
-                                "length": "271",
-                                "description": ">1wxj_A mol:protein length:271  tryptophan synthase alpha chain",
-                                "seqres": "MTTLEAFAKARSEGRAALIPYLTAGFPSREGFLQAVEEVLPYADLLEIGLPYSDPLGDGPVIQRASELALRKGMSVQGALELVREVRALTEKPLFLMTYLNPVLAWGPERFFGLFKQAGATGVILPDLPPDEDPGLVRLAQEIGLETVFLLAPTSTDARIATVVRHATGFVYAVSVTGVTGMRERLPEEVKDLVRRIKARTALPVAVGFGVSGKATAAQAAVADGVVVGSALVRALEEGRSLAPLLQEIRQGLQRLEANPGLKESSKKPLP",
-                                "ligands": ["3-(INDOL-3-YL)PROPYL PHOSPHATE"],
-                                img: "http://www.rcsb.org/pdb/images/1ksr_asr_r_250.jpg"
-                            }, {
-                                "pdb": "1wxj",
-                                "chain": "A",
-                                "length": "271",
-                                "description": ">1wxj_A mol:protein length:271  tryptophan synthase alpha chain",
-                                "seqres": "MTTLEAFAKARSEGRAALIPYLTAGFPSREGFLQAVEEVLPYADLLEIGLPYSDPLGDGPVIQRASELALRKGMSVQGALELVREVRALTEKPLFLMTYLNPVLAWGPERFFGLFKQAGATGVILPDLPPDEDPGLVRLAQEIGLETVFLLAPTSTDARIATVVRHATGFVYAVSVTGVTGMRERLPEEVKDLVRRIKARTALPVAVGFGVSGKATAAQAAVADGVVVGSALVRALEEGRSLAPLLQEIRQGLQRLEANPGLKESSKKPLP",
-                                "ligands": ["3-(INDOL-3-YL)PROPYL PHOSPHATE"],
-                                img: "http://www.rcsb.org/pdb/images/1ksr_asr_r_250.jpg"
-                            }]
-                    }]
-//                    [
-//                    {
-//                        masterID: '101mA',
-//                        masterDbID: 24322,
-//                        score: 1,
-//                        ident: 99,
-//                        flex: 5,
-//                        rmsd: 2.467,
-//                        size: 3,
-//                        representatives: [
-//                            {pdbid: '102m', selected: true, hasLigand: true, img: "http://www.rcsb.org/pdb/images/1ksr_asr_r_250.jpg"},
-//                            {pdbid: '104m', img: "http://www.rcsb.org/pdb/images/1ksr_asr_r_250.jpg"},
-//                            {pdbid: '111m', img: "http://www.rcsb.org/pdb/images/1ksr_asr_r_250.jpg"}
-//                        ]
-//                    },
-//                    {
-//                        masterID: '102mA',
-//                        masterDbID: 24322,
-//                        score: 1,
-//                        ident: 99,
-//                        flex: 5,
-//                        rmsd: 2.467,
-//                        size: 10,
-//                        representatives: [
-//                            {pdbid: '102m', hasLigand: true, img: "http://www.rcsb.org/pdb/images/1ksr_asr_r_250.jpg"},
-//                            {pdbid: '104m', img: "http://www.rcsb.org/pdb/images/1ksr_asr_r_250.jpg"},
-//                            {pdbid: '111m', selected: true, img: "http://www.rcsb.org/pdb/images/1ksr_asr_r_250.jpg"},
-//                            {pdbid: '102m', img: "http://www.rcsb.org/pdb/images/1ksr_asr_r_250.jpg"},
-//                            {pdbid: '104m', hasLigand: true, img: "http://www.rcsb.org/pdb/images/1ksr_asr_r_250.jpg"},
-//                            {pdbid: '111m', img: "http://www.rcsb.org/pdb/images/1ksr_asr_r_250.jpg"},
-//                            {pdbid: '111m', img: "http://www.rcsb.org/pdb/images/1ksr_asr_r_250.jpg"},
-//                            {pdbid: '102m', img: "http://www.rcsb.org/pdb/images/1ksr_asr_r_250.jpg"},
-//                            {pdbid: '104m', hasLigand: true, img: "http://www.rcsb.org/pdb/images/1ksr_asr_r_250.jpg"},
-//                            {pdbid: '111m', img: "http://www.rcsb.org/pdb/images/1ksr_asr_r_250.jpg"}
-//                        ]
-//                    },
-//                    {
-//                        masterID: '103mA',
-//                        masterDbID: 24322,
-//                        score: 1,
-//                        ident: 99,
-//                        flex: 5,
-//                        rmsd: 2.467,
-//                        size: 4,
-//                        representatives: [
-//                            {pdbid: '102m', active: true, img: "http://www.rcsb.org/pdb/images/1ksr_asr_r_250.jpg"},
-//                            {pdbid: '104m', img: "http://www.rcsb.org/pdb/images/1ksr_asr_r_250.jpg"},
-//                            { pdbid: '111m', img: "http://www.rcsb.org/pdb/images/1ksr_asr_r_250.jpg"},
-//                            { pdbid: '102m', img: "http://www.rcsb.org/pdb/images/1ksr_asr_r_250.jpg"},
-//                            { pdbid: '104m', img: "http://www.rcsb.org/pdb/images/1ksr_asr_r_250.jpg"}
-//
-//                        ]
-//                    }
-//
-//                ]
+
+            var req = {
+                method: 'POST',
+                url: 'http://modflex/phps/mastersBySequence.php',
+                data : {sequence:testSeq},
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                cache: $templateCache
             };
 
-            $scope.r = result;
+            $http(req).then(function successCallback(response) {
+                // this callback will be called asynchronously
+                // when the response is available
+                console.log(response);
+                $scope.r.hits = response.data;
+            }, function errorCallback(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
+//            // replace this json with server script call later
+//            var result = {
+//                id: 12345,
+//                hits: [{
+//                        "PDB": "1ujp",
+//                        "chain": "A",
+//                        "masterID": "1ujpA",
+//                        "masterDbID": "13292",
+//                        "score": "4.000e-26",
+//                        "ident": 32.02,
+//                        "flex": 5,
+//                        "rmsd": 2.467,
+//                        "size": 2,
+//                        "representatives": [{
+//                                "pdb": "1wxj",
+//                                "chain": "A",
+//                                "length": "271",
+//                                "description": ">1wxj_A mol:protein length:271  tryptophan synthase alpha chain",
+//                                "seqres": "MTTLEAFAKARSEGRAALIPYLTAGFPSREGFLQAVEEVLPYADLLEIGLPYSDPLGDGPVIQRASELALRKGMSVQGALELVREVRALTEKPLFLMTYLNPVLAWGPERFFGLFKQAGATGVILPDLPPDEDPGLVRLAQEIGLETVFLLAPTSTDARIATVVRHATGFVYAVSVTGVTGMRERLPEEVKDLVRRIKARTALPVAVGFGVSGKATAAQAAVADGVVVGSALVRALEEGRSLAPLLQEIRQGLQRLEANPGLKESSKKPLP",
+////                                "ligands": ["3-(INDOL-3-YL)PROPYL PHOSPHATE"],
+//                                img: "http://www.rcsb.org/pdb/images/1ksr_asr_r_250.jpg"
+//                            }, {
+//                                "pdb": "1wxj",
+//                                "chain": "A",
+//                                "length": "271",
+//                                "description": ">1wxj_A mol:protein length:271  tryptophan synthase alpha chain",
+//                                "seqres": "MTTLEAFAKARSEGRAALIPYLTAGFPSREGFLQAVEEVLPYADLLEIGLPYSDPLGDGPVIQRASELALRKGMSVQGALELVREVRALTEKPLFLMTYLNPVLAWGPERFFGLFKQAGATGVILPDLPPDEDPGLVRLAQEIGLETVFLLAPTSTDARIATVVRHATGFVYAVSVTGVTGMRERLPEEVKDLVRRIKARTALPVAVGFGVSGKATAAQAAVADGVVVGSALVRALEEGRSLAPLLQEIRQGLQRLEANPGLKESSKKPLP",
+//                                "ligands": ["3-(INDOL-3-YL)PROPYL PHOSPHATE"],
+//                                img: "http://www.rcsb.org/pdb/images/1ksr_asr_r_250.jpg"
+//                            }]
+//                    }, {
+//                        "PDB": "1ujp",
+//                        "chain": "A",
+//                        "masterID": "1ujpA",
+//                        "masterDbID": "13292",
+//                        "score": "4.000e-26",
+//                        "ident": 32.02,
+//                        "flex": 5,
+//                        "rmsd": 2.467,
+//                        "size": 2,
+//                        "representatives": [{
+//                                "pdb": "1wxj",
+//                                "chain": "A",
+//                                "length": "271",
+//                                "description": ">1wxj_A mol:protein length:271  tryptophan synthase alpha chain",
+//                                "seqres": "MTTLEAFAKARSEGRAALIPYLTAGFPSREGFLQAVEEVLPYADLLEIGLPYSDPLGDGPVIQRASELALRKGMSVQGALELVREVRALTEKPLFLMTYLNPVLAWGPERFFGLFKQAGATGVILPDLPPDEDPGLVRLAQEIGLETVFLLAPTSTDARIATVVRHATGFVYAVSVTGVTGMRERLPEEVKDLVRRIKARTALPVAVGFGVSGKATAAQAAVADGVVVGSALVRALEEGRSLAPLLQEIRQGLQRLEANPGLKESSKKPLP",
+//                                "ligands": ["3-(INDOL-3-YL)PROPYL PHOSPHATE"],
+//                                img: "http://www.rcsb.org/pdb/images/1ksr_asr_r_250.jpg"
+//                            }, {
+//                                "pdb": "1wxj",
+//                                "chain": "A",
+//                                "length": "271",
+//                                "description": ">1wxj_A mol:protein length:271  tryptophan synthase alpha chain",
+//                                "seqres": "MTTLEAFAKARSEGRAALIPYLTAGFPSREGFLQAVEEVLPYADLLEIGLPYSDPLGDGPVIQRASELALRKGMSVQGALELVREVRALTEKPLFLMTYLNPVLAWGPERFFGLFKQAGATGVILPDLPPDEDPGLVRLAQEIGLETVFLLAPTSTDARIATVVRHATGFVYAVSVTGVTGMRERLPEEVKDLVRRIKARTALPVAVGFGVSGKATAAQAAVADGVVVGSALVRALEEGRSLAPLLQEIRQGLQRLEANPGLKESSKKPLP",
+//                                "ligands": ["3-(INDOL-3-YL)PROPYL PHOSPHATE"],
+//                                img: "http://www.rcsb.org/pdb/images/1ksr_asr_r_250.jpg"
+//                            }]
+//                    }]
+//            };
+//
+//            $scope.r = result;
 
 
             $scope.getSelected = function () {
