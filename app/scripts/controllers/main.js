2e8cf13e104de4aaae9e7b77dd4c50c772a67547
Mayya Sedova <mayya.sedova@gmail.com>
2016-02-08 10:08:16 -0800

    First version of query history support.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -20,19 +20,58 @@ angular.module('modFlexApp')
                 return viewLocation === $location.path();
             };
         }])
-    .controller('AppCtrl', ['$scope', '$location', function ($scope, $location) {
+    .controller('AppCtrl', ['$scope', '$location', 'localStorageService',
+        function ($scope, $location, localStorageService) {
             // controller for the whole app
             // include getting list of jobs from local storage/db later
             $scope.querySequence = '';
             $scope.testFasta = ">1A50:A|PDBID|CHAIN|SEQUENCE\nMERYENLFAQLNDRREGAFVPFVTLGDPGIEQSLKIIDTLIDAGADALELGVPFSDPLADGPTIQNANLRAFAAGVTPAQCFEMLALIREKHPTIPIGLLMYANLVFNNGIDAFYARCEQVGVDSVLVADVPVEESAPFRQAALRHNIAPIFICPPNADDDLLRQVASYGRGYTYLLSRSGVTGAENRGALPLHHLIEKLKEYHAAPALQGFGISSPEQVSAAVRAGAAGAISGSAIVKIIEKNLASPKQMLAELRSFVSAMKAASRA";
 
+            var queriesSaved = localStorageService.get('queries');
+
+            $scope.queries = queriesSaved || [];
+
+            $scope.$watch('queries', function () {
+                localStorageService.set('queries', $scope.queries);
+            }, true);
 
             $scope.search = function () {
+                $scope.queries.push({title: $scope.titleFromSeq($scope.querySequence),
+                    sequence: $scope.querySequence});
                 $location.path('search');
             };
 
+            $scope.clear = function () {
+                $scope.querySequence = '';
+            };
+
+            $scope.titleFromSeq = function (seq) {
+                var idx = seq.indexOf('\n');
+                var title;
+                if (idx >= 1) {
+                    title = seq.substr(0, idx);
+                } else {
+                    title = seq.substr(0, 17) + '...';
+                }
+                return title;
+            }
+
             $scope.useTestFasta = function () {
                 $scope.querySequence = $scope.testFasta;
+            };
+
+            $scope.useQuery = function(index){
+                var userObject = $scope.queries[index];
+                $scope.querySequence = userObject.sequence;
+                $location.path('search');
+
+            };
+
+            $scope.removeQuery = function (index) {
+                $scope.queries.splice(index, 1);
+            };
+            $scope.clearRecentQueries = function () {
+                $scope.queries = [];
             };
         }])
     ;
