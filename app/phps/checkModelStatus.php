4bd239345a44fb96f59e30bff9300867c4b51f53
Thomas Hrabe <thrabe@gmail.com>
2016-02-09 11:57:21 -0800

    checking modeling works script copies URL over

---
+++ app/phps/checkModelStatus.php
@@ -0,0 +1,35 @@
+<?php
+
+	header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Credentials: true ");
+	header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+	header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size,     X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
+	header('Content-Type: application/json');
+
+	$modelJobID = $_GET['modelID'];
+	
+	$url = "http://ffas.godziklab.org/protmod2-cgi/check_protmod2.pl";
+
+	$data = array('jobId' => $modelJobID);
+	// print_r($data);
+	// use key 'http' even if you send the request to https://...
+	$options = array(
+	    'http' => array(
+	        // 'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+	        'method'  => 'POST',
+	        'content' => http_build_query($data),
+	    ),
+	);
+
+	$context  = stream_context_create($options);
+	$result = file_get_contents($url, false, $context);
+
+	$resultObject = json_decode($result, TRUE);
+
+	if(strcmp($resultObject["Status"] , "Done") == 0){
+		$url = $resultObject["url"];
+		file_put_contents("../jobs/model.pdb", file_get_contents($url));
+	}
+	// {"JobId": "query.436554299.24757"}, {"Status": "Progress"}}
+	echo($result);
+?>
