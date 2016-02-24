0c98e196076efd17875da4fd5df6e7788a249f09
Thomas Hrabe <thrabe@gmail.com>
2016-02-24 10:05:53 -0800

    trying angularJS parameter hack for GET

--- app/phps/checkModelStatus.php
+++ app/phps/checkModelStatus.php
@@ -6,8 +6,12 @@
 	header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
 	header('Content-Type: application/json');
 
-	$modelJobID = $_GET['modelID'];
-	$sessionID = $_GET['sessionID'];
+
+	$getdata = file_get_contents("php://input");
+	$request = json_decode($getdata);
+
+	$modelJobID = $request->modelID;
+	$sessionID 	= $request->sessionID;
 
 	if(is_null($modelJobID) || is_null($sessionID)){
 		$errorMessage = array();
