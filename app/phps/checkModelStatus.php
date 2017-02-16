357cf766b65b2b075a7cff4a71d264d39c5c1082
godziklab <modflex@modflex.burnham.org>
2017-02-15 16:32:56 -0800

    simplified modeling request

--- app/phps/checkModelStatus.php
+++ app/phps/checkModelStatus.php
@@ -40,17 +40,7 @@
 	$resultObject = json_decode($result, TRUE);
 
 	if(strcmp($resultObject["Status"] , "Done") == 0){
-		$url = $resultObject["url"];
-
-		$sessionFolder = "../jobs/".$sessionID;
-		if(!is_dir($sessionFolder))
-			mkdir($sessionFolder);
-
-		file_put_contents($sessionFolder."/".$modelJobID."model.pdb", file_get_contents($url));
-
-		$resultObject["url"] = "/jobs/".$sessionID."/".$modelJobID."model.pdb";
 		echo(json_encode($resultObject));
-
 	}else{
 		// {"JobId": "query.436554299.24757"}, {"Status": "Progress"}}
 		echo($result);
